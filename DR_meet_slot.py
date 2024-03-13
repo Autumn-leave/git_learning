1f908d6b3f1ab71b23f1b3d987ef22c983aaaea8
meena <meenachivalliammai@gmail.com>
2024-03-13 08:20:54 +0530

    commit 2

--- DR_meet_slot.py
+++ DR_meet_slot.py
@@ -4,4 +4,7 @@ import random
 l = ['Antony Joseph Raj','Ashika M','Disha Matilda','Gayathri G S','Gorli Lavanya','Keerthika Kannan','Kundan Kumar','Manojkumar','Mariappan Subbiah','Meenachi S','Pooja Thakur','Praveenkumar S','Sachin DS','Sai Kiran Nalluri','Sanjai R','Sathish Kumar','Sudheer Koyyana','Vajeeha Mohammed','Vivek V']
 
 # Printing the person name using randdom generator
-print("The celebrity who is taking MOM for the DR meeting is ",random.choice(l))
+#new comment
+#2nd comment
+#3rd comment
+print("The celebrity who is taking MOM for the Delivery meet meeting is ",random.choice(l))
