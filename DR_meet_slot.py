2d20d98b2587e41215af8936cf5024c0523bb784
prasanth <prasanth.s@weareplanet.com>
2024-03-13 07:43:49 +0530

    1st commit

---
+++ DR_meet_slot.py
@@ -0,0 +1,7 @@
+import random
+
+# Name list of team
+l = ['Antony Joseph Raj','Ashika M','Disha Matilda','Gayathri G S','Gorli Lavanya','Keerthika Kannan','Kundan Kumar','Manojkumar','Mariappan Subbiah','Meenachi S','Pooja Thakur','Praveenkumar S','Sachin DS','Sai Kiran Nalluri','Sanjai R','Sathish Kumar','Sudheer Koyyana','Vajeeha Mohammed','Vivek V']
+
+# Printing the person name using randdom generator
+print("The celebrity who is taking MOM for the DR meeting is ",random.choice(l))
---
+++ Roster.py
@@ -0,0 +1,5 @@
+import random
+
+l = ['Achyuth', 'Bahram', 'Gerard', 'Karthi', 'Meena', 'Nandhini', 'Toby']
+
+print("Today's legend for phrase of the day is",random.choice(l))
